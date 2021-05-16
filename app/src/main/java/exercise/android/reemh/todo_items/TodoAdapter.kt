7f03eb4fa9ebc48a627d64972d20f3a2ddd6d848
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-16 21:59:29 +0300

    Add adapter class for recycler view

---
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoAdapter.kt
@@ -0,0 +1,18 @@
+package exercise.android.reemh.todo_items
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter: RecyclerView.Adapter<TodoItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TodoItemHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
