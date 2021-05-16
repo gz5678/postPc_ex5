582674071a7ca3fa59e9234d760f7ed61b5542cd
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-16 22:07:44 +0300

    Add setTodos method and getItemCount

--- app/src/main/java/exercise/android/reemh/todo_items/TodoAdapter.kt
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoAdapter.kt
@@ -4,12 +4,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter: RecyclerView.Adapter<TodoItemHolder>() {
+
+    private val _todos: MutableList<TodoItem> = ArrayList()
+
+    fun setTodos(todos: List<TodoItem>) {
+        _todos.clear()
+        _todos.addAll(todos)
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemHolder {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return _todos.size
     }
 
     override fun onBindViewHolder(holder: TodoItemHolder, position: Int) {
