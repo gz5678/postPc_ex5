0218a75c701295ec46feca2349f323b572ce11f8
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-18 01:14:21 +0300

    Add comparable interface to TodoItem

--- app/src/main/java/exercise/android/reemh/todo_items/TodoItem.kt
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItem.kt
@@ -4,7 +4,14 @@ import java.io.Serializable
 
 data class TodoItem(val description : String,
                     val timestampCreated : Long,
-                    var status : Status) : Serializable
+                    var status : Status) : Serializable, Comparable<TodoItem>
 {
-    enum class Status {IN_PROGRESS, DONE}
+    enum class Status(val value: Int) {IN_PROGRESS(1), DONE(2)}
+
+    override fun compareTo(other: TodoItem): Int {
+        if (this.status == other.status) {
+            return -(this.timestampCreated.compareTo(other.timestampCreated))
+        }
+        return (this.status.value.compareTo(other.status.value))
+    }
 }
