94eab2ba17d34dc6ee059bebc6d84b409b7ff27f
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-18 01:54:38 +0300

    Created sorted data structure for TodoItemsHolderImpl

--- app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.kt
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.kt
@@ -1,9 +1,21 @@
 package exercise.android.reemh.todo_items
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 class TodoItemsHolderImpl : TodoItemsHolder {
-    private val todoItemArrayList = mutableListOf<TodoItem>()
+
+    // We want the collection to remain sorted after add and remove.
+    class SortedTodoItems: ArrayList<TodoItem>() {
+        override fun add(element: TodoItem): Boolean {
+            val index = this.binarySearch(element)
+            // binarySearch returns invertedInsertion point so need to transform to insertion point
+            super.add(-(index+1), element)
+            return true
+        }
+    }
+
+    private val todoItemArrayList = SortedTodoItems()
 
     override fun getCurrentItems(): MutableList<TodoItem> {
         return todoItemArrayList
